4756ad302e13e8fbccb194afd0bf87b12c77334b
dusklight00 <r.rahul.developer@gmail.com>
2023-11-15 03:17:22 +0530

    chakra ui setup complete

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading, Text, Box, Flex } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}
+    >
+      <Box maxW="2xl" m="0 auto">
+        <Heading as="h1" textAlign="center" fontSize="5xl" mt="100px">
+          Welcome Everybody!
+        </Heading>
+        <Text fontSize="xl" textAlign="center" mt="30px">
+          Tutorials and step-by-step guides from CodingTheSmartWay. Learn how to
+          build web applications!
+        </Text>
+        <Text
+          w="fit-content"
+          p="4"
+          px="50px"
+          bg="blue.300"
+          borderRadius="10px"
+          m="0 auto"
+          mt="8"
+          fontWeight="bold"
+          color="white"
+          fontSize="xl"
+        >
+          Get Started
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
